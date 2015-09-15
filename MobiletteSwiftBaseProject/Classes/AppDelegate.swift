02d5d4f7e2e4c159d32514ab5a640a99d0a319c9
Romain ASNAR <romain.asnar@gmail.com>
2015-09-15 17:20:49 +0200

    Setup project with Pods and VIPER.

--- MobiletteSwiftBaseProject/Classes/AppDelegate.swift
+++ MobiletteSwiftBaseProject/Classes/AppDelegate.swift
@@ -13,9 +13,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        if let window = self.window {
+            let rootWireframe = RootWireframe()
+            rootWireframe.presentRootViewController(fromWindow: window)
+        }
         return true
     }
 
